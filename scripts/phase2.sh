2e3a52bc90f2537642632d35df64f00a914d1133
ScoDay <scoday@gmx.com>
2017-03-28 07:24:50 +0900

    Adding some of the RHOSP stuff I did.

---
+++ scripts/phase2.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+# Phase Two:
+
+
+epel() {
+    # Download the latest release of epel this will include instack-undercloud.
+    wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+
+}
+
+
+
+
+export NODE_DIST=rhel7
+export NODE_CPU=4
+export UNDERCLOUD_NODE_CPU=4
+export UNDERCLOUD_NODE_MEM=12288
+export NODE_MEM=12288
+export DIB_LOCAL_IMAGE=rhel-guest-image-7.2.x86_64.qcow2
+export REG_METHOD=portal
+export REG_USER="rhn-user"
+export REG_PASSWORD="password"
+export REG_POOL_ID="8a85f9833e1404a9013e3cddf95a0599"
+export REG_REPOS="rhel-7-server-rpms rhel-7-server-extras-rpms rhel-7-server-openstack-8-rpms rhel-7-server-openstack-8-director-rpms rhel-7-server-rh-common-rpms"export NODE_COUNT=5
+export LIBVIRT_VOL_POOL=tripleo
+export LIBVIRT_VOL_POOL_TARGET=/home/vm_storage_pool
